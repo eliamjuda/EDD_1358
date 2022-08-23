d54eff8c4bf24777c736617d5e079bc527097d76
eliamjuda <eliamjuda@gmail.com>
2022-08-22 22:30:59 -0500

    Final

--- tarea1/script.js
+++ tarea1/script.js
@@ -1,13 +1,14 @@
+const $ = selector => document.querySelector(selector)
 
-const $form = document.getElementById("form");
-const $csv = document.getElementById("csv");
-const $firstInput = document.getElementById("inputOne");
-const $inputTwo = document.getElementById("inputTwo");
-const $threeInput = document.getElementById("inputThree");
-const $fourInput = document.getElementById("inputFour");
-const $firstMonth = document.getElementById("firstMonth");
-const $secondMonth = document.getElementById("secondMonth");
-const $monthSubmit = document.getElementById("monthSubmit");
+const $form = $('#form'),
+    $csv = $('#csv'),
+    $firstInput = $("#inputOne"),
+    $inputTwo = $("#inputTwo"),
+    $threeInput = $("#inputThree"),
+    $fourInput = $("#inputFour"),
+    $firstMonth = $("#firstMonth"),
+    $secondMonth = $("#secondMonth"),
+    $monthSubmit = $("#monthSubmit");
 
 function csvArray(csvContent, csvDelimiter = "," ){
     const headers = csvContent.slice(0, csvContent.indexOf("\n")).split(csvDelimiter);
@@ -35,25 +36,26 @@ $form.addEventListener("submit", function(e){
         const text = e.target.result;
         const data = csvArray(text);
         console.log(data);
-        $firstInput.value = parseInt(data[7].JUNIO.match(/(\d+)/g)) - parseInt(data[7].ENERO.match(/(\d+)/g)) + " seguidores";
+        $firstInput.value = parseInt(data[7].JUNIO) - parseInt(data[7].ENERO) + " seguidores";
 
-        const threeInputValue = ( ( parseInt(data[1].JUNIO.match(/(\d+)/g)) -  parseInt(data[1].ENERO.match(/(\d+)/g)) ) / parseInt(data[1].ENERO.match(/(\d+)/g)) * 100 ).toFixed(2);
-        const fourInputValue = ( ( parseInt(data[8].JUNIO.match(/(\d+)/g)) -  parseInt(data[8].ENERO.match(/(\d+)/g)) ) / parseInt(data[8].ENERO.match(/(\d+)/g)) * 100 ).toFixed(2);
+        const threeInputValue = ( ( parseInt(data[1].JUNIO) -  parseInt(data[1].ENERO) ) / parseInt(data[1].ENERO) * 100 ).toFixed(2);
+        const fourInputValue = ( ( parseInt(data[8].JUNIO) -  parseInt(data[8].ENERO) ) / parseInt(data[8].ENERO) * 100 ).toFixed(2);
 
         $threeInput.value = "facebook " + threeInputValue + "%";
         $fourInput.value = "twitter " + fourInputValue + "%";
 
         $firstMonth.addEventListener("change", (e) => {
-            const arrMonth = [];
+            
+            $inputTwo.value = parseInt(data[15][$secondMonth.value]) - parseInt(data[15][$firstMonth.value]);
+        })
 
-            arrMonth.push($firstMonth.value);
-            arrMonth.push($secondMonth.value);
-
-            $inputTwo.value = data[15].arrMonth[0];
+        $secondMonth.addEventListener("change", (e) => {
+            
+            $inputTwo.value =  parseInt(data[15][$secondMonth.value]) - parseInt(data[15][$firstMonth.value]);
         })
 
     };
-    
+
     reader.readAsText(input);
 
 });
